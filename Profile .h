5e9ab84580ed6063b3c0ecf9a1ac15b76307ee3f
build-crypto <build23705@gmail.com>
2026-03-22 19:04:33 +0700

    Create Profile .h

---
+++ Profile .h
@@ -0,0 +1,16 @@
+#ifndef PROFILE_H
+#define PROFILE_H
+
+
+class Profile
+{
+    public:
+        void Nhap();
+        void XepHang();
+    private:
+        char  NAME[31];
+        double dVan,dToan,dTB;
+
+};
+
+#endif // PROFILE_H
